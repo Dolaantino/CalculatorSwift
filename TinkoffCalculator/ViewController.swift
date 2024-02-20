30862f719f768b4067aff574a516861f817c4922
Johny <89834351+Dolaantino@users.noreply.github.com>
2024-02-20 15:42:57 +0300

    calc

--- TinkoffCalculator/ViewController.swift
+++ TinkoffCalculator/ViewController.swift
@@ -7,12 +7,58 @@
 
 import UIKit
 
+enum CalculationError: Error {
+    case dividedByZero
+}
+
+enum Operation: String {
+    case add = "+"
+    case substruct = "-"
+    case multiply = "X"
+    case divide = "/"
+    
+    func calculate(_ number1: Double, _ number2: Double) throws -> Double {
+        switch self {
+        case .add:
+            return number1 + number2
+        case .substruct:
+            return number1 - number2
+        case .multiply:
+            return number1 * number2
+        case .divide:
+            if number2 == 0 {
+                throw CalculationError.dividedByZero
+            }
+            return number1 / number2
+        }
+    }
+}
+
+enum CalculationHistoryItem {
+    case number(Double)
+    case operation(Operation)
+}
+
 class ViewController: UIViewController {
+    
+    lazy var numberFormatter: NumberFormatter = {
+        let numberFormatter = NumberFormatter()
+        
+        numberFormatter.usesGroupingSeparator = false
+        numberFormatter.locale = Locale(identifier: "ru_RU")
+        numberFormatter.numberStyle = .decimal
+        
+        return numberFormatter
+    }()
+
+    var calculationHistory: [CalculationHistoryItem] = []
+    
+    @IBOutlet weak var label: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print("ТАДАМММ")
+        resetLabelText()
     }
     
     @IBAction func buttonPressed(_ sender: UIButton) {
@@ -20,7 +66,77 @@ class ViewController: UIViewController {
             print("Error")
             return
         }
-        print(buttonText)
+        if buttonText == "," && label.text?.contains(",") == true {
+            return
+        }
+        if label.text == "0" {
+            label.text = buttonText
+        } else {
+            label.text?.append(buttonText)
+        }
+    }
+    
+    @IBAction func operationButtonPressed(_ sender: UIButton) {
+        guard 
+            let buttonText = sender.currentTitle,
+            let buttonOperation = Operation(rawValue: buttonText)
+        else { return }
+        
+        guard
+            let labelText = label.text,
+            let labelNumber = numberFormatter.number(from: labelText)?.doubleValue
+        else { return }
+        
+        calculationHistory.append(.number(labelNumber))
+        calculationHistory.append(.operation(buttonOperation))
+        
+        resetLabelText()
+    }
+    
+    @IBAction func clearButtonPressed() {
+        calculationHistory.removeAll()
+        
+        resetLabelText()
+    }
+    
+    @IBAction func calculateButtonPressed() {
+        guard
+            let labelText = label.text,
+            let labelNumber = numberFormatter.number(from: labelText)?.doubleValue
+        else { return }
+        
+        calculationHistory.append(.number(labelNumber))
+        
+        do {
+            let result = try calculate()
+            
+            label.text = numberFormatter.string(from: NSNumber(value: result))
+        } catch {
+            label.text = "Ошибка"
+        }
+        
+        calculationHistory.removeAll()
+    }
+    
+    func resetLabelText() {
+        label.text = "0"
+    }
+    
+    func calculate() throws -> Double {
+        guard case .number(let firstNumber) = calculationHistory[0] else { return 0 }
+        
+        var currentResult = firstNumber
+        
+        for index in stride(from: 1, to: calculationHistory.count - 1, by: 2) {
+            guard 
+                case .operation (let operation) = calculationHistory[index],
+                case .number(let number) = calculationHistory[index + 1]
+                else { break }
+            
+            currentResult =  try operation.calculate(currentResult, number)
+        }
+        
+        return currentResult
     }
 }
 
